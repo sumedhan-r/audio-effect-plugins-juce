9e449c3813dddf70f604ef4fc7041c56693a8e59
Sumedhan Ramesh <sumedhanramesh2012@gmail.com>
2026-01-02 00:31:53 +0900

    Applying pre-commit

--- 01_Learn_Delay/Source/PluginEditor.cpp
+++ 01_Learn_Delay/Source/PluginEditor.cpp
@@ -6,35 +6,33 @@
   ==============================================================================
 */
 
-#include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "PluginProcessor.h"
 
 //==============================================================================
-_01_Learn_DelayAudioProcessorEditor::_01_Learn_DelayAudioProcessorEditor (_01_Learn_DelayAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
-{
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+_01_Learn_DelayAudioProcessorEditor::_01_Learn_DelayAudioProcessorEditor(
+    _01_Learn_DelayAudioProcessor &p)
+    : AudioProcessorEditor(&p), audioProcessor(p) {
+  // Make sure that before the constructor has finished, you've set the
+  // editor's size to whatever you need it to be.
+  setSize(400, 300);
 }
 
-_01_Learn_DelayAudioProcessorEditor::~_01_Learn_DelayAudioProcessorEditor()
-{
-}
+_01_Learn_DelayAudioProcessorEditor::~_01_Learn_DelayAudioProcessorEditor() {}
 
 //==============================================================================
-void _01_Learn_DelayAudioProcessorEditor::paint (juce::Graphics& g)
-{
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (juce::Colours::blueviolet);    // change_1
+void _01_Learn_DelayAudioProcessorEditor::paint(juce::Graphics &g) {
+  // (Our component is opaque, so we must completely fill the background with a
+  // solid colour)
+  g.fillAll(juce::Colours::blueviolet); // change_1
 
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (40.0f));  // change_2
-    g.drawFittedText ("My first plug-in", getLocalBounds(), juce::Justification::centred, 1);   // change_3
+  g.setColour(juce::Colours::white);
+  g.setFont(juce::FontOptions(40.0f)); // change_2
+  g.drawFittedText("My first plug-in", getLocalBounds(),
+                   juce::Justification::centred, 1); // change_3
 }
 
-void _01_Learn_DelayAudioProcessorEditor::resized()
-{
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+void _01_Learn_DelayAudioProcessorEditor::resized() {
+  // This is generally where you'll want to lay out the positions of any
+  // subcomponents in your editor..
 }
--- 01_Learn_Delay/Source/PluginEditor.h
+++ 01_Learn_Delay/Source/PluginEditor.h
@@ -8,26 +8,26 @@
 
 #pragma once
 
-#include <JuceHeader.h>
 #include "PluginProcessor.h"
+#include <JuceHeader.h>
 
 //==============================================================================
 /**
-*/
-class _01_Learn_DelayAudioProcessorEditor  : public juce::AudioProcessorEditor
-{
+ */
+class _01_Learn_DelayAudioProcessorEditor : public juce::AudioProcessorEditor {
 public:
-    _01_Learn_DelayAudioProcessorEditor (_01_Learn_DelayAudioProcessor&);
-    ~_01_Learn_DelayAudioProcessorEditor() override;
+  _01_Learn_DelayAudioProcessorEditor(_01_Learn_DelayAudioProcessor &);
+  ~_01_Learn_DelayAudioProcessorEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+  //==============================================================================
+  void paint(juce::Graphics &) override;
+  void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    _01_Learn_DelayAudioProcessor& audioProcessor;
+  // This reference is provided as a quick way for your editor to
+  // access the processor object that created it.
+  _01_Learn_DelayAudioProcessor &audioProcessor;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (_01_Learn_DelayAudioProcessorEditor)
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(
+      _01_Learn_DelayAudioProcessorEditor)
 };
